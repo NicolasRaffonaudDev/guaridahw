62ed5cad63d22a5c3f3135866fd9a94a31694d54
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2024-10-12 15:57:29 -0300

    confirmacion

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -5,7 +5,7 @@ const ItemDetail = ( {id, name, img, description, price} ) => {
     <div key={id} className="card align-items-center text-center p-3 my-3" style={{width: 300}}>
             <img src={img} style={{width: 200, height:200}} alt={name} className="card-img-top"/>
             <div className="card-body">
-                <h5 className="card-title">asdasd{name}</h5>
+                <h5 className="card-title">{name}</h5>
                 <p className="card-text">{description}</p>
                 <h2 className="card-text">$ {price}</h2>
                 <Link to="" className="btn btn-primary">
