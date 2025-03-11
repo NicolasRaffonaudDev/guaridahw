5d409dcf69a6c754289011be958535ed5d24b4a5
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2025-03-11 20:16:25 -0300

    Optimizaciones de carga de imagenes, refactorizacion de servicios de conexion con firestore y creacion de scripts

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -14,21 +14,30 @@ function Carousel() {
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active" data-bs-interval="3000">
-            <img src={slide1} className="d-block w-100" alt="slide1" />
+            <picture>
+              <source srcSet={slide1} type="image/webp" />
+              <img src={slide1} className="d-block w-100" alt="slide1" loading="lazy" />
+            </picture>
             <div className="carousel-caption d-none d-md-block">
               <h5>First slide label</h5>
               <p>Some representative placeholder content for the first slide.</p>
             </div>
           </div>
           <div className="carousel-item" data-bs-interval="3000">
-            <img src={slide2} className="d-block w-100" alt="slide2" />
+            <picture>
+              <source srcSet={slide2} type="image/webp" />
+              <img src={slide2} className="d-block w-100" alt="slide2" />
+            </picture>
             <div className="carousel-caption d-none d-md-block">
               <h5>Second slide label</h5>
               <p>Some representative placeholder content for the second slide.</p>
             </div>
           </div>
           <div className="carousel-item" data-bs-interval="3000">
-            <img src={slide3} className="d-block w-100" alt="slide3" />
+            <picture>
+              <source srcSet={slide3} type="image/webp" />
+              <img src={slide3} className="d-block w-100" alt="slide3" />
+            </picture>
             <div className="carousel-caption d-none d-md-block">
               <h5>Third slide label</h5>
               <p>Some representative placeholder content for the third slide.</p>
