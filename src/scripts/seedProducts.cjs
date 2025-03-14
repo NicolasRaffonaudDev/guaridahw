c49dbd35df030733f6d5b1be1e7c62eb7e12121a
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2025-03-14 12:56:31 -0300

    feat: optimizacion de estilos y carga de imagenes nuevas

--- src/scripts/seedProducts.cjs
+++ src/scripts/seedProducts.cjs
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
-const products = require('./products.json');
+const products = require('../data/products.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
